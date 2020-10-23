baacaeaab71e08c8e10e3b03441031021915d3ce
Kirsten Szeto <kszeto20@stuy.edu>
2020-10-23 00:02:38 -0400

    I tested this. It works

--- Demo.java
+++ Demo.java
@@ -10,6 +10,22 @@ public class Demo {
       a = Integer.parseInt(args[0]);
     }
     printLoop(a);
+
+    int[] one = {1};
+    int[] two = {1,2};
+    int[] three = {1,2,3};
+    int[] four = {1,2,3,4};
+    int[] five = {1,2,3,4,5};
+
+    int[][] w = { {1,2,3}, {4,5,6}, {2} };
+
+    System.out.println(arrToString(one));
+    System.out.println(arrToString(two));
+    System.out.println(arrToString(three));
+    System.out.println(arrToString(four));
+    System.out.println(arrToString(five));
+
+    System.out.println(arrayDeepToString(w));
   }
 
 
@@ -21,4 +37,31 @@ public class Demo {
       System.out.println();
     }
   }
+
+public static String arrToString(int[] arr){
+  String newString = "{";
+  if (arr.length > 0){
+  for (int i = 0; i < arr.length - 1; i++){
+    newString = newString + Integer.toString(arr[i]) + ", ";
+  }
+  newString = newString + Integer.toString(arr[arr.length - 1]);
+  }
+  return newString + "}";
+}
+
+public static String arrayDeepToString(int[][] arr){
+  String newString = "{ \n";
+  for (int i = 0; i < arr.length; i++){
+    newString = newString + "{";
+    for (int j = 0; j < arr[i].length; j++){
+      if (j == arr[i].length - 1){
+        newString = newString + arr[i][j];
+      }
+      else newString = newString + arr[i][j] + ", ";
+    }
+    newString = newString + "}";
+    newString = newString + "\n";
+  }
+  return newString + "}";
+}
 }
